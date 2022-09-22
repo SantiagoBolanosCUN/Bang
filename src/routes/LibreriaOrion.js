1ea11c277bf39c05cfcd2ce783a1e941f12ca8f4
matbui <u6000223@unimilitar.edu.co>
2022-09-22 12:21:39 -0500

    constelacion libreria orion

--- src/routes/LibreriaOrion.js
+++ src/routes/LibreriaOrion.js
@@ -1,5 +1,95 @@
 import React from "react";
+import "../components/libreriaOrion.css";
 
 export default function LibreriaOrion() {
-  return <div> Libreria Orion</div>;
+  return (
+    <div className=" h-screen">
+      <div class="constelation">
+        <h1>Librería Orión</h1>
+        <p>En cada estrella encontraras una sorpresa</p>
+      </div>
+
+      <div class="galaxy">
+        <div class="estrella">
+          <a href="/">
+            <div class="dot">
+              <div class="theev">Nombre del evento - Fecha </div>
+            </div>
+          </a>
+          <div class="event">
+            <a href="/">
+              <div class="dentr1">
+                <div class="theev2">Nombre del evento - Fecha</div>
+              </div>
+            </a>
+          </div>
+          <a href="/">
+            <div class="dot">
+              <div class="theev">Nombre del evento - Fecha </div>
+            </div>
+          </a>
+          <div class="event2">
+            <a href="/">
+              <div class="dentr2">
+                <div class="theev3">Nombre del evento - Fecha</div>
+              </div>
+            </a>
+          </div>
+          <a href="/">
+            <div class="dot">
+              <div class="theev">Nombre del evento - Fecha </div>
+            </div>
+          </a>
+          <div class="event">
+            <a href="/">
+              <div class="dentr1">
+                <div class="theev2">Nombre del evento - Fecha</div>
+              </div>
+            </a>
+          </div>
+          <a href="/">
+            <div class="dot">
+              <div class="theev">Nombre del evento - Fecha </div>
+            </div>
+          </a>
+          <div class="event2">
+            <a href="/">
+              <div class="dentr2">
+                <div class="theev3">Nombre del evento - Fecha</div>
+              </div>
+            </a>
+          </div>
+          <a href="/">
+            <div class="dot">
+              <div class="theev">Nombre del evento - Fecha </div>
+            </div>
+          </a>
+          <div class="event">
+            <a href="/">
+              <div class="dentr1">
+                <div class="theev2">Nombre del evento - Fecha</div>
+              </div>
+            </a>
+          </div>
+          <a href="/">
+            <div class="dot">
+              <div class="theev">Nombre del evento - Fecha </div>
+            </div>
+          </a>
+          <div class="event2">
+            <a href="/">
+              <div class="dentr2">
+                <div class="theev3">Nombre del evento - Fecha</div>
+              </div>
+            </a>
+          </div>
+          <a href="/">
+            <div class="dot">
+              <div class="theev">Nombre del evento - Fecha </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
