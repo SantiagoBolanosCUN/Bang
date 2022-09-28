014ab09334c5045925d880bb96a32d29a9294318
matbui <u6000223@unimilitar.edu.co>
2022-09-28 18:29:53 -0500

    sliders

---
+++ src/components/swiperImg.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+// react-id-swiper
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+// custom css
+import "./HeroSlider.css";
+// imagenes
+import Libreria2 from "../assets/Libreria2.png";
+import Libreria3 from "../assets/Libreria3.png";
+import Libreria4 from "../assets/Libreria4.png";
+import Libreria5 from "../assets/Libreria5.png";
+import Libreria6 from "../assets/Libreria6.png";
+import Libreria6A from "../assets/Libreria6A.png";
+import Libreria11 from "../assets/Libreria11.png";
+import Libreria11A from "../assets/Libreria11A.png";
+import Libreria12 from "../assets/Libreria12.png";
+import Libreria13 from "../assets/Libreria13.png";
+import Libreria14 from "../assets/Libreria14.png";
+
+const SwiperImg = (/*numeroSlide*/) => {
+  return (
+    <>
+      <div
+        style={{
+          width: "90%",
+          height: "90%",
+          overflow: "hidden",
+          marginTop: "5vw",
+          marginLeft: "5vw",
+          position: "relative",
+        }}
+      >
+        <Swiper
+          style={{
+            "--swiper-navigation-color": "black",
+            "--swiper-navigation-size": "1.3rem",
+          }}
+          centeredSlides={true}
+          autoplay={{
+            delay: 10500,
+            disableOnInteraction: false,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          initialSlide={0 /*numeroSlide*/}
+        >
+          <SwiperSlide>
+            <img src={Libreria2} alt="image1" style={{ width: "100%" }}></img>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <img src={Libreria3} alt="image2" style={{ width: "100%" }}></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Libreria4} alt="image3" style={{ width: "100%" }}></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Libreria5} alt="image4" style={{ width: "100%" }}></img>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <img src={Libreria6} alt="image5" style={{ width: "100%" }}></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Libreria6A} alt="image6" style={{ width: "100%" }}></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Libreria11} alt="image7" style={{ width: "100%" }}></img>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <img src={Libreria11A} alt="image8" style={{ width: "100%" }}></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Libreria12} alt="image9" style={{ width: "100%" }}></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Libreria13} alt="image10" style={{ width: "100%" }}></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Libreria14} alt="image11" style={{ width: "100%" }}></img>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
+};
+
+export default SwiperImg;
--- src/routes/LibreriaOrion.js
+++ src/routes/LibreriaOrion.js
@@ -1,95 +1,109 @@
 import React from "react";
 import "../components/libreriaOrion.css";
 
-export default function LibreriaOrion() {
-  return (
-    <div className=" h-screen">
-      <div class="constelation">
-        <h1>Librería Orión</h1>
-        <p>En cada estrella encontraras una sorpresa</p>
-      </div>
+import { useState } from "react";
+import Modal from "../components/modal";
 
-      <div class="galaxy">
-        <div class="estrella">
-          <a href="/">
-            <div class="dot">
-              <div class="theev">Nombre del evento - Fecha </div>
-            </div>
-          </a>
-          <div class="event">
-            <a href="/">
-              <div class="dentr1">
-                <div class="theev2">Nombre del evento - Fecha</div>
+export default function LibreriaOrion() {
+  const [modalOn, setModalOn] = useState(false);
+  const [choice, setChoice] = useState(false);
+  const ref = useState();
+
+  function clicked() {
+    setModalOn(true);
+  }
+
+  return (
+    <>
+      {/* conditionally display the result of the action if user confirms  */}
+      {choice && <div className="flex justify-center"></div>}
+
+      {modalOn && <Modal setModalOn={setModalOn} setChoice={setChoice} />}
+      <div className=" h-screen">
+        <div className="constelation">
+          <h1>Librería Orión</h1>
+          <p>En cada estrella encontraras una sorpresa</p>
+        </div>
+
+        <div className="galaxy">
+          <div className="estrella">
+            <div className="event2"></div>
+            <button ref={ref} id={0} onClick={clicked}>
+              <div className="dot">
+                <div className="theev">Nombre del evento - Fecha </div>
               </div>
-            </a>
-          </div>
-          <a href="/">
-            <div class="dot">
-              <div class="theev">Nombre del evento - Fecha </div>
+            </button>
+
+            <div className="event3">
+              <button ref={ref} id={1} onClick={clicked}>
+                <div className="dot">
+                  <div className="theev">Nombre del evento - Fecha </div>
+                </div>
+              </button>
             </div>
-          </a>
-          <div class="event2">
-            <a href="/">
-              <div class="dentr2">
-                <div class="theev3">Nombre del evento - Fecha</div>
+
+            <div className="event5">
+              <button ref={ref} id={2} onClick={clicked}>
+                <div className="dot">
+                  <div className="theev">Nombre del evento - Fecha </div>
+                </div>
+              </button>
+            </div>
+          </div>
+          <div className="estrella">
+            <div className="event5">
+              <button ref={ref} id={3} onClick={clicked}>
+                <div className="dentr1">
+                  <div className="theev">Nombre del evento - Fecha </div>
+                </div>
+              </button>
+            </div>
+            <button ref={ref} id={4} onClick={clicked}>
+              <div className="dot">
+                <div className="theev">Nombre del evento - Fecha </div>
               </div>
-            </a>
-          </div>
-          <a href="/">
-            <div class="dot">
-              <div class="theev">Nombre del evento - Fecha </div>
-            </div>
-          </a>
-          <div class="event">
-            <a href="/">
-              <div class="dentr1">
-                <div class="theev2">Nombre del evento - Fecha</div>
+            </button>
+            <div className="event"></div>
+            <button ref={ref} id={5} onClick={clicked}>
+              <div className="dot">
+                <div className="theev">Nombre del evento - Fecha </div>
               </div>
-            </a>
-          </div>
-          <a href="/">
-            <div class="dot">
-              <div class="theev">Nombre del evento - Fecha </div>
-            </div>
-          </a>
-          <div class="event2">
-            <a href="/">
-              <div class="dentr2">
-                <div class="theev3">Nombre del evento - Fecha</div>
+            </button>
+            <div className="event"></div>
+            <button ref={ref} id={6} onClick={clicked}>
+              <div className="dot">
+                <div className="theev">Nombre del evento - Fecha </div>
               </div>
-            </a>
+            </button>
           </div>
-          <a href="/">
-            <div class="dot">
-              <div class="theev">Nombre del evento - Fecha </div>
+          <div className="estrella">
+            <div className="event4">
+              <button ref={ref} id={7} onClick={clicked}>
+                <div className="drent1">
+                  <div className="theev">Nombre del evento - Fecha </div>
+                </div>
+              </button>
             </div>
-          </a>
-          <div class="event">
-            <a href="/">
-              <div class="dentr1">
-                <div class="theev2">Nombre del evento - Fecha</div>
+            <button ref={ref} id={8} onClick={clicked}>
+              <div className="dot">
+                <div className="theev">Nombre del evento - Fecha </div>
               </div>
-            </a>
-          </div>
-          <a href="/">
-            <div class="dot">
-              <div class="theev">Nombre del evento - Fecha </div>
-            </div>
-          </a>
-          <div class="event2">
-            <a href="/">
-              <div class="dentr2">
-                <div class="theev3">Nombre del evento - Fecha</div>
+            </button>
+            <div className="event6"></div>
+            <button ref={ref} id={9} onClick={clicked}>
+              <div className="dot">
+                <div className="theev">Nombre del evento - Fecha </div>
               </div>
-            </a>
+            </button>
+            <div className="event2"></div>
+            <button ref={ref} id={10} onClick={clicked}>
+              <div className="dot">
+                <div className="theev">Nombre del evento - Fecha </div>
+              </div>
+            </button>
           </div>
-          <a href="/">
-            <div class="dot">
-              <div class="theev">Nombre del evento - Fecha </div>
-            </div>
-          </a>
         </div>
       </div>
-    </div>
+    </>
   );
 }
