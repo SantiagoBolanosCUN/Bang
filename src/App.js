6214cc271abca3b17707dd896e666ba0666e7f8a
matbui <u6000223@unimilitar.edu.co>
2022-10-11 09:30:23 -0500

    routeo login

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ import Supernova from "./routes/Supernova";
 import Ideaverso from "./routes/Ideaverso";
 import LibreriaOrion from "./routes/LibreriaOrion";
 import MeteoroGame from "./routes/MeteoroGame";
+import Login from "./routes/Login";
 
 export default function App() {
   return (
     <div className=" h-screen">
       <ThreeRouter history={history}>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Login />} />
+          <Route path="/Home" element={<Home />} />
           <Route path="/detail/:plantetId" element={<Detail />} />
           <Route path="/NebulosaInnova" element={<NebulosaInnova />} />
           <Route path="/Supernova" element={<Supernova />} />
---
+++ src/routes/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div>
+      <a className="btn" href="/Home">
+        Home
+      </a>
+    </div>
+  );
+}
